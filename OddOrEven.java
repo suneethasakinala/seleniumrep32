81ae158d7842e9bba44741253934d97b88e7074d
suneethasakinala <128903355+suneethasakinala@users.noreply.github.com>
2023-03-30 17:04:51 +0530

    Add files via upload
    oddOr even.java

---
+++ OddOrEven.java
@@ -0,0 +1,19 @@
+
+public class OddOrEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int num=15;
+       
+       if(num%2!=0) {
+    	   
+       System.out.println("The given number is odd");
+       
+       }
+       else {
+    	   System.out.println("The given number is even");
+       }
+       
+	}
+
+}
