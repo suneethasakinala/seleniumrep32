529e8338d1931f5bd5680b2121cf9b42edd7739c
suneethasakinala <128903355+suneethasakinala@users.noreply.github.com>
2023-03-30 17:02:35 +0530

    Add files via upload
    Tab.java

---
+++ Tab.java
@@ -0,0 +1,32 @@
+
+public class Tab {
+
+	public int Tabprice() {
+          int Tabprice= 10000;
+		return Tabprice;
+		
+	}
+	public String TabbrandName() {
+		
+		String TabbrandName="brandName";
+		return TabbrandName;
+		
+		
+	}
+	
+	public static void main(String[] args) {
+	
+		Tab lenovo=new Tab();
+		lenovo.Tabprice();
+		lenovo.TabbrandName();{
+			System.out.println(10000);
+			System.out.println("lenovo");
+		}
+		
+		
+		Tab samsung=new Tab();
+		samsung.Tabprice();
+		samsung.TabbrandName(); 
+	}
+
+}
