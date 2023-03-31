0d7b05408a849be3f2002174f52dab20314b53cc
suneethasakinala <128903355+suneethasakinala@users.noreply.github.com>
2023-03-31 20:45:29 +0530

    Add files via upload
    student.java

---
+++ Student.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Student {
+	public static void main(String[] args) {
+		
+}
+
+        String studentName= "goutham";
+		int rollNo= 20;
+		String collegeName= "vasavi";
+		short marksScored=79;
+		float cgpa=7.9f;
+	
+			// TODO Auto-generated method stub
+		
+			
+		}
+
