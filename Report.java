5cb005448c8e3bf592923b25917051da8ffa110f
suneethasakinala <128903355+suneethasakinala@users.noreply.github.com>
2023-03-31 20:46:40 +0530

    Add files via upload
    Report.java

---
+++ Report.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+	
+		Student stu= new Student();
+		String studentName=stu.studentName;
+		int studentrollNo=stu.rollNo;
+		String studentcollegeName=stu.collegeName;
+		short studentmarksScored=stu.marksScored;
+		float studentcgpa=stu.cgpa;
+		System.out.println("studentName="+studentName);
+		System.out.println("studentrollNo="+studentrollNo);
+		System.out.println("studentcollegeName="+studentcollegeName);
+		System.out.println("student marksScored="+studentmarksScored);
+		System.out.println("student cgpa="+studentcgpa);
+		
+		
+
+		
+
+	}
+
+}
